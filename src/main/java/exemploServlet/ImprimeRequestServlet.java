0ea1f697448bd6e7f07923a84bc27bebf91b066e
alunoces <alunoces@L5MAQ10.labcesac.net>
2017-08-21 21:55:54 -0300

    terminando Servlet de impressão.

--- src/main/java/exemploServlet/ImprimeRequestServlet.java
+++ src/main/java/exemploServlet/ImprimeRequestServlet.java
@@ -1,7 +1,9 @@
 package exemploServlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,23 +12,40 @@ import javax.servlet.http.HttpServletResponse;
 public class ImprimeRequestServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ImprimeRequestServlet</title>");
+            out.println("<title>Imprime Servlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<p>Método: "+request.getMethod()+"</p>");
-            out.println("<p>Recurso: "+request.getContextPath()+"</p>");
-            out.println("<p>URL completa: "+request.getRequestURL()+"</p>");
-            out.println("<p>Parametros de busca: "+request.getQueryString()+"</p>");
+            out.println("<h1>Imprime Request</h1>");
+            out.println("<p>Método: " + request.getMethod() + "</p>");
+            out.println("<p>Recurso: " + request.getRequestURI() + "</p>");
+            out.println("<p>URL completa: " + request.getRequestURL() + "</p>");
+            out.println("<p>Parametros de busca: " + request.getQueryString() + "</p>");
             out.println("<p>Cabeçalhos:</p>");
+            Enumeration<String> cabecalhos = request.getHeaderNames();
+            out.println("     <ul>");
+            while (cabecalhos.hasMoreElements()) {
+                String cabecalho = cabecalhos.nextElement();
+                String valor = request.getHeader(cabecalho);
+                out.println("     <li>" + cabecalho + ": " + valor + "</li>");
+            }
+            out.println("     </ul>");
+
+            out.println("     <p>Corpo:</p>");
+
+            BufferedReader reader = request.getReader();
+                String linha;
+                while ((linha = reader.readLine()) != null) {
+                    out.println(linha + "<br/>");
+            }
             out.println("</body>");
             out.println("</html>");
         }
