3cb7ee1f8b67bd0aa85f17279d6f7b72888b3688
alunoces <alunoces@L5MAQ10.labcesac.net>
2017-08-28 22:17:41 -0300

    editando listSevlet

--- src/main/java/exemploServlet/ListServlet.java
+++ src/main/java/exemploServlet/ListServlet.java
@@ -12,19 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ListServlet extends HttpServlet {
 
+    private List<Tarefa> tarefas = new ArrayList();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String descricao = request.getParameter("descricao");
-        boolean valor = "on".equals(request.getParameter("valor"));
+        boolean valor = "ON".equals(request.getParameter("valor"));
         Tarefa tarefa = new Tarefa();
-        tarefa.setDescrição("Exemplo de tarefa");
-        tarefa.setDescrição("Tarefa 3");
-        tarefa.setDescrição("Outra tarefa");
         tarefa.setDescrição(descricao);
-        ArrayList<Tarefa> tarefas = new ArrayList();
+
         tarefas.add(tarefa);
-        
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -32,19 +31,23 @@ public class ListServlet extends HttpServlet {
             out.println("<html>");
             out.println("<head>");
             out.println("<title>To DO Dinâmico</title>");
-            out.println("<style>");
-            out.println("p ,li, h2, h1 {");
-            out.println("font-family: cursive;");
-            out.println("font-style: oblique;}");
-            out.println("</style>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>To DO Dinâmico</h1>");
             out.println("<h2>Lista de tarefas web dinâmica</h2>");
             out.println("<p>Insira suas tarefas na lista abaixo e controle o que precisa fazer</p>");
             out.println("<ul>");
-            for(int i = 0 ;i < tarefas.size(); i++){
-            out.println("<li>"+tarefas.get(i).getDescrição()+"</li>");
+            out.println("<li>Exemplo de tarefa</li>");
+            out.println("<li>Tarefa 3</li>");
+            out.println("<li>Outra tarefa</li>");
+            if (valor == true) {
+                for (int i = 0; i < tarefas.size(); i++) {
+                    out.println("<strong><li>" + tarefas.get(i).getDescrição() + "</li></strong>");
+                }
+            } else {
+                for (int i = 0; i < tarefas.size(); i++) {
+                    out.println("<li>" + tarefas.get(i).getDescrição() + "</li>");
+                }
             }
             out.println("</ul>");
             out.println("<hr>");
